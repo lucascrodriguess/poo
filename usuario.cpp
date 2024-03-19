e275e15619099a758864948ec1788fe44ce7f519
lucascrodriguess <164095754+lucascrodriguess@users.noreply.github.com>
2024-03-19 20:12:34 -0300

    Add files via upload

---
+++ usuario.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class Usuario {
+
+    //atributos
+private:
+    string nome;
+    int idade;
+    arquivo *imagem
+    string endereço
+
+    //metodos
+    public:
+        void setIdade (int idade){
+            this->idade = idade;     
+        }
+        
+        int getIdade (){
+            return idade;
+        }
+
+        void setNome (string nome){
+            this->nome = nome;
+        }
+
+};
---
+++ usuariomain.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    Usuario meuUsuario;
+
+        meuUsuario.setIdade (19);
+
+        cout << meuUsuario.getIdade () >>;
+
+    return 0;
+}
