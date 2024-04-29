fc7322b25a08d666d16466448bde1909de88c1b9
Beatriz Machado <beatrizrolandmachado@gmail.com>
2024-04-29 15:06:09 -0300

    atualização dos contrutores

--- tarefa 3/Caminhao.h
+++ tarefa 3/Caminhao.h
@@ -6,8 +6,6 @@ class Caminhao : public Veiculo {
 
     public:
         Caminhao ();
-        Caminhao (string modelo);
-        Caminhao (string modelo, int capacidadeDeCarga);
         Caminhao (string modelo, int capacidadeDeCarga, int anoDeFabricacao, int volume, int peso, double localizacaoLatitude, double localizacaoLongitude);
         ~Caminhao ();
         
