6cb59f519fa90a130d16ae0de60f369bde42506f
Beatriz Machado <beatrizrolandmachado@gmail.com>
2024-04-29 13:56:41 -0300

    Correção da Classe Clientes
    - adição de verificações nas funções set;
    - adição de funções get;
    - correção de ambos os construtores, pois o 1 nao implementava nenhum valor default e o 2 colocava os valores diretamente nos atributos, logo as verificações seriam inúteis.

--- tarefa 3/Cliente.cpp
+++ tarefa 3/Cliente.cpp
@@ -2,40 +2,108 @@
 
 using namespace std;
 
-    Cliente::Cliente (){}
+    Cliente::Cliente (){
+        this->nome = "";
+        this->cpf = "";
+        this->telefone = 0;
+        this->endereco = "";
+        this->email = "";
+    }
     
-    Cliente::Cliente (string nome, int cpf, int telefone, string endereco, string email){
-        this->nome = nome;
-        this->cpf = cpf;
-        this->telefone = telefone;
-        this->endereco = endereco;
-        this->email = email;
+    Cliente::Cliente (string nome, string cpf, int telefone, string endereco, string email){
+        setNome(nome);
+        setCPF(cpf);
+        setTelefone(telefone);
+        setEndereco(endereco);
+        setEmail(email);
     }
 
     Cliente::~Cliente (){}
-    
 
     int Cliente::setNome (string nome){
+        for(auto &n : nome) {
+            if(!isalpha(n)) {
+                return 0;
+            }
+        }
         this->nome = nome;
-        return 1;
+        return 1;               
     }
-    int Cliente::setCPF (int cpf){
+
+    int Cliente::setCPF (string cpf){
+        int contador = 0;
+        for(auto &n : cpf) {
+            contador++;
+            if(!isdigit(n)) {
+                return 0;
+            }
+        }
+
+        if(contador != 11) {
+            return 0;
+        }
         this->cpf = cpf;
         return 1;
     }
+
     int Cliente::setTelefone (int telefone){
         this->telefone = telefone;
         return 1;
     }
+
     int Cliente::setEndereco (string endereco){
         this->endereco = endereco;
         return 1;
     }
+
     int Cliente::setEmail (string email){
+        int verificador = 0;
+        for(auto &Char : email) {
+            if (Char == '@') {
+                verificador++;
+            }
+        }
+
+        if (verificador != 1) {
+            return 0;
+        }
+
         this->email = email;
         return 1;
     }
 
-    string Cliente::getNome (){
-        return this -> nome;
+    string Cliente::getNome () {
+        if(this->nome.empty()) {
+            return "Nome invalido!";
+        }
+        return this->nome;
+    }
+
+    string Cliente::getCPF () {
+        if(this->cpf.empty()) {
+            return "Cpf invalido!";
+        }
+        return this->cpf;
+    }
+
+    int Cliente::getTelefone () {
+        if(this->telefone == 0) {
+            printf("Cpf Invalido");
+            return 0;
+        }
+        return this->telefone;
+    }
+
+    string Cliente::getEndereco () {
+        if(this->endereco.empty()) {
+            return "Endereco invalido!";
+        }
+        return this->endereco;
+    }
+
+    string Cliente::getEmail () {
+        if(this->email.empty()) {
+            return "Email invalido!";
+        }
+        return this->email;
     }
--- tarefa 3/Cliente.h
+++ tarefa 3/Cliente.h
@@ -5,21 +5,25 @@ class Cliente {
 
     private:
         std::string nome;
-        int cpf;
+        std::string cpf;
         int telefone;
         std::string endereco;
         std::string email;
 
     public:
         Cliente ();
-        Cliente (std::string nome, int cpf, int telefone, std::string endereco, std::string email);
+        Cliente (std::string nome, std::string cpf, int telefone, std::string endereco, std::string email);
         ~Cliente ();
 
         int setNome (std::string nome);
-        int setCPF (int cpf);
+        int setCPF (std::string cpf);
         int setTelefone (int telefone);
         int setEndereco (std::string endereco);
         int setEmail (std::string email);
 
-        std::string getNome ();
+        std::string getNome();
+        std::string getCPF();
+        int getTelefone();
+        std::string getEndereco();
+        std::string getEmail();
 };
