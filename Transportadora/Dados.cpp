54bddc1acd2c042b702f6bf0c198375fd9c8f409
Beatriz Machado <beatrizrolandmachado@gmail.com>
2024-06-04 14:38:25 -0300

    update
    leitura e armazenamento de dados do arquivo .csv funcionando

--- Transportadora/Dados.cpp
+++ Transportadora/Dados.cpp
@@ -23,32 +23,34 @@ void Dados::LerArquivoCSV(const std::string arquivo) {
     if (!file.is_open()) {
         throw std::runtime_error("Erro ao abrir o arquivo");
         return;
-    } else std::getline(file, linha); // pula a linha de titulos da tabela
-
-    while (std::getline(file, linha)) {
-        linha = ArrumaLinha(file, linha);
-        std::cout<<"linha completa..." << linha << std::endl;
-        SeparaeArmazenaDados(linha, ',');
+    } else {
+        getline(file, linha); // pula a linha de titulos da tabela
+        std::cout<<"Arquivo aberto!" << std::endl;
     }
 
+    while (getline(file, linha)) {
+        //std::cout<<"linha parcial: " << linha << std::endl;
+        linha = ArrumaLinha(file, linha);
+        //std::cout<<"linha completa: " << linha << std::endl;
+        SeparaeArmazenaDados(linha, ','); 
+    }
+    std::cout<<"Arquivo fechado!\n" << std::endl;
     file.close();
 }
 
 std::string Dados::ArrumaLinha(std::ifstream &x, std::string line) {
     
     int quebra_de_linha = 0;
-    while (!line.empty() && line.back() == '\n' && quebra_de_linha < 6) {
-        line.pop_back();
-        quebra_de_linha++;
-        
+    std::string resultado = line;
+    while (!line.empty() && quebra_de_linha < 6) {
         std::string continuacao;
-        if (std::getline(x, continuacao)) {
-            line += continuacao;
-        } else {
+        if(!getline(x, continuacao)){
             break;
         }
+        resultado += continuacao;
+        quebra_de_linha++;
     }
-    return line;
+    return resultado;
 }
 
 void Dados::SeparaeArmazenaDados(const std::string linha, char x) {
@@ -62,48 +64,67 @@ void Dados::SeparaeArmazenaDados(const std::string linha, char x) {
 
     int quebra_de_linha = 0;
 
-    do{ std::getline(lineStream, conteudo_atributo, x);
-        std::cout<<"conteudo_atributo: " << conteudo_atributo << std::endl;
+    do{ 
+        getline(lineStream, conteudo_atributo, x);
+        //std::cout<<"conteudo_atributo: " << conteudo_atributo << std::endl;
+        std::string conteudo2;
+
+        int flag = 0;
+        
+        //std::cout<<"virgulas: " << virgulas << std::endl;
+        //std::cout<<"flag: " << flag << std::endl;
 
         switch (virgulas) {
         case 0:
-            std::cout<<"nome valido" << std::endl;
+            //std::cout<<"nome valido" << std::endl;
             novoCliente->setNome(conteudo_atributo);
             break;
         case 1:
-            std::cout<<"cpf valido" << std::endl;
+            //std::cout<<"cpf valido" << std::endl;
             novoCliente->setCPF(conteudo_atributo);
             break;
         case 2:
-            std::cout<<"telefone valido" << std::endl;
-            novoCliente->setTelefone(conteudo_atributo);
+            //std::cout<<"endereco valido" << std::endl;
+            if(conteudo_atributo.back() == '"') {
+                conteudo2 = novoCliente->getEndereco() + ",";
+                novoCliente->setEndereco(conteudo2+=conteudo_atributo);
+                virgulas++; 
+            } else novoCliente->setEndereco(conteudo_atributo);
             break;
         case 3:
-            std::cout<<"endereco valido" << std::endl;
-            novoCliente->setEndereco(conteudo_atributo);
+            //std::cout<<"telefone valido" << std::endl;
+            novoCliente->setTelefone(conteudo_atributo);
             break;
         case 4:
-            std::cout<<"email valido" << std::endl;
+            //std::cout<<"email valido" << std::endl;
             novoCliente->setEmail(conteudo_atributo);
             break;
         case 5:
-            std::cout<<"endereco coleta valido" << std::endl;
-            novoPedido->setEnderecoColeta(conteudo_atributo);
+            //std::cout<<"endereco coleta valido" << std::endl;
+            if(conteudo_atributo.back() == '"') {
+                conteudo2 = novoPedido->getEnderecoColeta() + ",";
+                novoPedido->setEnderecoColeta(conteudo2+=conteudo_atributo);
+                virgulas++; 
+            } else novoPedido->setEnderecoColeta(conteudo_atributo);
             break;
         case 6:
-            std::cout<<"endereco entrega valido" << std::endl;
-            novoPedido->setEnderecoEntrega(conteudo_atributo);
+            //std::cout<<"endereco entrega valido" << std::endl;
+            if(conteudo_atributo.back() == '"') {
+                conteudo2 = novoPedido->getEnderecoEntrega() + ",";
+                novoPedido->setEnderecoEntrega(conteudo2+=conteudo_atributo);
+                virgulas++; 
+            } else novoPedido->setEnderecoEntrega(conteudo_atributo);
             break;
         case 7:
-            std::cout<<"peso valido" << std::endl;
+            //std::cout<<"peso valido" << std::endl;
             novoPedido->setPesoDaCarga(std::stoi(conteudo_atributo));
             break;
         case 8:
-            std::cout<<"volume valido" << std::endl;
+            //std::cout<<"volume valido" << std::endl;
             novoPedido->setVolumeDaCarga(std::stoi(conteudo_atributo));
             break;
         case 9:
-            std::cout<<"prioridade valido" << std::endl;
+            //std::cout<<"prioridade valido" << std::endl;
             novoPedido->setPrioridade(conteudo_atributo);
             break;
         default:
@@ -111,11 +132,14 @@ void Dados::SeparaeArmazenaDados(const std::string linha, char x) {
             break;
         }
 
-        /*if (conteudo_atributo.front() != '"' && conteudo_atributo.back() != '"') {
-            virgulas++;
-        }*/
+        for(auto caractere : conteudo_atributo) {
+            if(caractere == '"') {
+                flag = 1;
+                break;
+            }
+        }
 
-        if (virgulas != 2 && virgulas != 5 && virgulas != 6) {
+        if(flag == 0) {
             virgulas++;
         }
         
@@ -124,5 +148,7 @@ void Dados::SeparaeArmazenaDados(const std::string linha, char x) {
     listaClientes->adicionaCliente(novoCliente);
     listaPedidos->adicionaPedidos(novoPedido);
 }
+
+
 
 
